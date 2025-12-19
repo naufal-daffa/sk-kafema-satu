2288c9ca31015ca5e991a61a0e14de601faec1cd
HabibieRaysyah <habibieresya@gmail.com>
2025-12-19 13:08:32 +0700

    Habibie

--- 10.js
+++ 10.js
@@ -1,7 +1,9 @@
 let nilai = [80, 76, 78, 82, 80, 91];
 
-nilai.sort(function(a, b) {
+nilai.sort(function (a, b) {
   return b - a;
 });
 
-console.log(nilai[0], nilai[1], nilai[2]);
+for (let i = 0; i < 3; i++) {
+  console.log(nilai[i]);
+}
--- 4.js
+++ 4.js
@@ -2,7 +2,13 @@ let angka = prompt("Masukkan angka");
 
 let panjang = angka.length;
 
-if (panjang == 1) alert("satuan");
-else if (panjang == 2) alert("puluhan");
-else if (panjang == 3) alert("ratusan");
-else alert("ribuan atau lebih");
+if (angka > 0) {
+  if (panjang == 1) alert("satuan");
+  else if (panjang == 2) alert("puluhan");
+  else if (panjang == 3) alert("ratusan");
+  else if (panjang == 4) alert("ribuan");
+  else if (panjang == 5) alert("puluhan ribuan");
+  else if (panjang == 6) alert("ratusan ribuan");
+  else if (panjang == 7) alert("jutaan");
+  else alert("puluhan jutaan atau lebih");
+}
